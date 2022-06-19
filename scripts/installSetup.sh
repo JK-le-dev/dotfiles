7d544894f713d048921abb6711b267b4fdd7e460
JK-le-dev <jkcondor13@gmail.com>
2022-06-18 19:57:26 -0500

    Added install script

---
+++ scripts/installSetup.sh
@@ -0,0 +1,24 @@
+#!/bin/bash
+
+#Lists of apps to download
+pacmanApps="yay htop trash-cli vim libreoffice imwheel grub-customizer piper ncdu"
+yayApps="onedriver pfetch snapd flatpak jdk17-openjdk"
+snapApps="brave joplin-desktop todoist spotify"
+snapClassicApps=("code" "intellij-idea-community")
+flatpakApps="yuzu"
+
+#Arch Repos
+sudo pacman -Syu $pacmanApps --noconfirm
+yay -S $yayApps --noconfirm
+
+#Setting up snap
+sudo systemctl enable --now snapd.socket
+sudo ln -s /var/lib/snapd/snap /snap
+
+#Repos other than Arch
+sudo snap install $snapApps
+sudo flatpak install $flatpakApps
+
+for app in $snapClassicApps; do
+	sudo snap install $app
+done
