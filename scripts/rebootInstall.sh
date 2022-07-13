8208c2dedf84681a8fac338b7979b72d266b4ac0
JK-le-dev <jkcondor13@gmail.com>
2022-07-13 05:28:10 +0000

    updated scripts

--- scripts/rebootInstall.sh
+++ scripts/rebootInstall.sh
@@ -1,32 +1,14 @@
 #!/bin/bash
 
 #Lists of apps to download
-pacmanApps="libreoffice-fresh htop trash-cli vim libreoffice-fresh imwheel grub-customizer piper ncdu gnome-keyring libgnome-keyring libsecret imagemagick alacritty tmux tree npm cmake"
-yayApps="onedriver pfetch snapd flatpak jdk17-openjdk rclone-bin rclone-browser pulseway-bin alacritty-themes konsave"
 snapApps="brave joplin-desktop todoist spotify"
 snapClassicApps=("code" "intellij-idea-community" "pycharm-community")
-
-getYay () {
-git clone https://aur.archlinux.org/yay.git
-cd yay
-makepkg -si
-cd ~
-}
-
-time-datectl set-local-rtc 0
-
-#Arch Repos
-getYay &&
-sudo pacman -Syu $pacmanApps --noconfirm &&
-yay -S $yayApps --noconfirm &&
-
-#Setting up snap
-sudo systemctl enable --now snapd.socket &&
-sudo ln -s /var/lib/snapd/snap /snap &&
+flatpak="yuzu"
 
 #Repos other than Arch
 sudo snap install $snapApps &&
+sudo flatpak install $flatpak && 
 
 for app in $snapClassicApps; do
-	sudo snap install $app
+	sudo snap install $app --classic
 done
