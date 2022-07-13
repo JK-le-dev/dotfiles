cd3d26ac88cb9ecc8b7e651600bd45be8a091700
JK-le-dev <jkcondor13@gmail.com>
2022-07-13 05:33:54 +0000

    wah

--- scripts/rebootInstall.sh
+++ scripts/rebootInstall.sh
@@ -1,14 +1,14 @@
 #!/bin/bash
 
 #Lists of apps to download
+snap
 snapApps="brave joplin-desktop todoist spotify"
-snapClassicApps=("code" "intellij-idea-community" "pycharm-community")
 flatpak="yuzu"
 
 #Repos other than Arch
-sudo snap install $snapApps &&
-sudo flatpak install $flatpak && 
-
-for app in $snapClassicApps; do
-	sudo snap install $app --classic
-done
+su
+snap install $snapApps &&
+flatpak install $flatpak && 
+snap install code --classic &&
+snap install intellij-idea-community --classic &&
+snap install pycharm-community --classic
